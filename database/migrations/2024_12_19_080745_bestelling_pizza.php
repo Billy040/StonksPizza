c2ea44bdf8c3c9ca3efd1b190b32ca97f81a5303
Billy Dekkers <ps237376@edu.summacollege.nl>
2025-01-21 16:24:28 +0100

    Migration edit

--- database/migrations/2024_12_19_080745_bestelling_pizza.php
+++ database/migrations/2024_12_19_080745_bestelling_pizza.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('bestelling_pizza', function (Blueprint $table) {
             $table->foreignId('bestelling_id')->constrained('bestelling');
             $table->foreignId('pizza_id')->constrained('pizza');
+            $table->integer('aantal');
             $table->primary(['bestelling_id', 'pizza_id']);
         });
     }
